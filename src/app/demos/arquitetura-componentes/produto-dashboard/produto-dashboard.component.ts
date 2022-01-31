27fd10ae1199cc8d6f28ffc6ecee4debc1a12956
CesarRibeiro <cesarribeiro@outlook.com>
2022-01-30 21:12:03 -0300

    Eventos

--- src/app/demos/arquitetura-componentes/produto-dashboard/produto-dashboard.component.ts
+++ src/app/demos/arquitetura-componentes/produto-dashboard/produto-dashboard.component.ts
@@ -1,15 +1,93 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { fromEvent, Observable } from 'rxjs';
+import { ProdutoDetalheComponent } from '../componentes/produto-card-detalhe.component';
+import { ProdutoCountComponent } from '../componentes/produto-cout.component';
+import { Produto } from '../models/produto';
 
 @Component({
   selector: 'app-produto-dashboard',
   templateUrl: './produto-dashboard.component.html',
   styles: []
 })
-export class ProdutoDashboardComponent implements OnInit {
+export class ProdutoDashboardComponent implements OnInit, AfterViewInit {
+
+  produtos: Produto[]
+
+  @ViewChild(ProdutoCountComponent, { static: false }) contador: ProdutoCountComponent;
+  @ViewChild('teste', { static: false }) mensagemTela: ElementRef;
+
+  @ViewChildren(ProdutoDetalheComponent) botoes: QueryList<ProdutoDetalheComponent>;
 
   constructor() { }
 
   ngOnInit() {
+    this.produtos = [{
+      id: 1,
+      nome: 'Teste',
+      ativo: true,
+      valor: 100,
+      imagem: 'celular.jpg'
+    },
+    {
+      id: 2,
+      nome: 'Teste 2',
+      ativo: true,
+      valor: 200,
+      imagem: 'gopro.jpg'
+    },
+    {
+      id: 3,
+      nome: 'Teste 3',
+      ativo: true,
+      valor: 300,
+      imagem: 'laptop.jpg'
+    },
+    {
+      id: 4,
+      nome: 'Teste 4',
+      ativo: true,
+      valor: 400,
+      imagem: 'mouse.jpg'
+    },
+    {
+      id: 5,
+      nome: 'Teste 5',
+      ativo: true,
+      valor: 500,
+      imagem: 'teclado.jpg'
+    },
+    {
+      id: 6,
+      nome: 'Teste 6',
+      ativo: false,
+      valor: 600,
+      imagem: 'headset.jpg'
+    }];
+
+  }
+
+  ngAfterViewInit(): void {
+      let clickTexto: Observable<any> = fromEvent(this.mensagemTela.nativeElement, 'click');
+      clickTexto.subscribe(() => {
+        alert('clicou no texto!');
+        return;
+      })
+
+      console.log('Objeto do contador: ', this.contador.produtos)
+
+      console.log(this.botoes);
+      this.botoes.forEach(p => {
+        this.verificaAtivo(p.produto.ativo)
+      })
+  }
+
+  mudarStatus(event: Produto){
+    event.ativo = !event.ativo;
+  }
+
+  verificaAtivo(ativo: boolean){
+    if(ativo) console.log('Status: Ativo');
+    else console.log('Status: Desligado');
   }
 
 }
--- src/app/demos/arquitetura-componentes/produto.module.ts
+++ src/app/demos/arquitetura-componentes/produto.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt);
+
 import { ProdutoDashboardComponent } from './produto-dashboard/produto-dashboard.component';
 import { ProdutoRoutingModule } from './produto.route';
+import { ProdutoDetalheComponent } from './componentes/produto-card-detalhe.component';
+import { ProdutoCountComponent } from './componentes/produto-cout.component';
 
 @NgModule({
     declarations: [
-        ProdutoDashboardComponent
+        ProdutoDashboardComponent,
+        ProdutoDetalheComponent,
+        ProdutoCountComponent
     ],
     imports: [
         CommonModule,
